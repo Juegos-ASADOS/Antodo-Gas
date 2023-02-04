e414f3f690fe4e54235d6885c531aab9de4f7c8a
diecasti <diecasti@ucm.es>
2023-02-04 04:02:29 +0100

    Editor Scripts

---
+++ Assets/Scripts/Tools/RamaExample.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RamaExample : MonoBehaviour
+{
+
+
+    public GameObject obstaculo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
