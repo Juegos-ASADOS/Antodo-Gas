5453f04938b322cd76e9103d9d994cd5477877e0
osgarc05 <osgarc05@ucm.es>
2023-02-04 06:02:11 +0100

    Cuidado que en mi escena hay juego

--- Assets/Scripts/DemoScripts/DemoCameraBezier.cs
+++ Assets/Scripts/DemoScripts/DemoCameraBezier.cs
@@ -9,8 +9,11 @@ public class DemoCameraBezier : MonoBehaviour
     public EndOfPathInstruction endOfPathInstruction;
     public float speed = 5;
     public float offset = 5;
+    public float pinchoPunch = 200;
+
     float distanceTravelled;
     float acumRot = 0;
+    float lateralAcceleration = 0;
 
     void Start()
     {
@@ -28,17 +31,19 @@ public class DemoCameraBezier : MonoBehaviour
             distanceTravelled += speed * Time.deltaTime;
             transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
             transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
-            transform.Rotate(0, 0, acumRot);
 
             if (Input.GetKey(KeyCode.D)) acumRot += 1;
             if (Input.GetKey(KeyCode.A)) acumRot -= 1;
 
+            acumRot += lateralAcceleration;
+            transform.Rotate(0, 0, acumRot);
             transform.position = transform.position + transform.up * offset;
+
+            if (lateralAcceleration > 0)
+                lateralAcceleration -= 0.1f;
+            else
+                lateralAcceleration = 0;
         }
-
-        if (Input.GetKey(KeyCode.W)) speed = 15;
-
-
     }
 
     // If the path changes during the game, update the distance travelled so that the follower's position on the new path
@@ -46,5 +51,13 @@ public class DemoCameraBezier : MonoBehaviour
     void OnPathChanged()
     {
         distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PinchoTrigger>() != null)
+        {
+            lateralAcceleration = pinchoPunch;
+        }
     }
 }
