bc8688075f58882c559016d279b149b7205d7389
sergimol <sergimol@ucm.es>
2023-02-04 15:10:28 +0100

    De 0 again

---
+++ Antodo Gas/Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    //Exit the game
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
