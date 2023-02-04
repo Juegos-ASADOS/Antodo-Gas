1923be46eae5310a7fc98932c40980b001243f3f
SergioJAlfonso <sergialf@ucm.es>
2023-02-04 20:07:02 +0100

    más online

--- Antodo Gas/Assets/Scripts/GameManager.cs
+++ Antodo Gas/Assets/Scripts/GameManager.cs
@@ -6,6 +6,12 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    const int maxPlayersInLobby = 4;
+
+    Dictionary<string, int> playersInLobby = new Dictionary<string, int>();
+
+    string currentLobby;
     void Awake()
     {
         if (instance == null)
@@ -17,20 +23,53 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
-    }  
+    }
     public void changeScene(string name)
     {
         SceneManager.LoadScene(name);
     }
-    // Start is called before the first frame update
-    void Start()
+    public string enterLobby(string key)
     {
-        
+        int numPlayers;
+        if (playersInLobby.TryGetValue(key, out numPlayers))
+        {
+            //SI no esta llena
+            if (numPlayers < 4)
+            {
+                playersInLobby.Remove(key);
+                playersInLobby.Add(key, numPlayers+1);
+                currentLobby = key;
+                return "Connecting to lobby...";
+            }
+            //Si esta llena
+            return "Lobby Full!";
+        }
+        else
+        {
+            //Si no existe
+            return "Lobby not found";
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public string createLobby(string key)
     {
-        
+        int numPlayers;
+        if (playersInLobby.TryGetValue(key, out numPlayers))
+        {
+            //SI ya existe
+            return "Lobby already exists!";
+        }
+        else
+        {
+            //Si no existe
+            playersInLobby.Add(key, 1);
+            currentLobby = key;
+            return "Creating Lobby...";
+        }
+    }
+    public int getNumPlayersInCurrentLobby()
+    {
+        int num;
+        playersInLobby.TryGetValue(currentLobby, out num);
+        return num;
     }
 }
